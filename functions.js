3c1c3d66012c42e1f5066330cf9f20a8f4197253
RocknCoder <rockncoder@gmail.com>
2015-12-05 15:47:30 -0800

    closures!

--- functions.js
+++ functions.js
@@ -10,8 +10,8 @@
     }
   };
 
-  function foo(thing1, thing2){
-    if(this && this.money){
+  function foo(thing1, thing2) {
+    if (this && this.money) {
       console.info("Money = " + this.money);
       console.info("thing1 = " + thing1 + ", thing2 = " + thing2)
     } else {
@@ -19,20 +19,20 @@
     }
   }
 
-  function sum(){
-    if(arguments.length > 0){
+  function sum() {
+    if (arguments.length > 0) {
       foo.apply({money: "$999"}, arguments);
     }
     var ndx, sum = 0;
-    for(ndx = 0; ndx < arguments.length; ndx += 1){
-      if(typeof arguments[ndx] === 'number'){
+    for (ndx = 0; ndx < arguments.length; ndx += 1) {
+      if (typeof arguments[ndx] === 'number') {
         sum += arguments[ndx];
       }
     }
     return sum;
   }
 
-  arr1.show = function(){
+  arr1.show = function () {
     console.info("Hello arr1 show");
   };
 
@@ -42,7 +42,7 @@
     this.y = y;
   }
 
-  Point.prototype.calc = function(){
+  Point.prototype.calc = function () {
     return x + y;
   }
 
@@ -51,7 +51,7 @@
     console.info("Hi from show1");
   }
 
-  show1.showbiz = function(){
+  show1.showbiz = function () {
     console.info("Hello from showbiz");
   };
 
@@ -68,24 +68,27 @@
   obj1.show1a(); // invoke as a method
   var myPoint = new Point(50, 100); // constructor
 
+  // using call and apply
+  // call takes a "this" and parameters to pass
   foo.call({money: "$100"}, "apple", "banana");
-  var arr2 = ["mango","durazno"];
+
+  // apply takes a "this" and
+  var arr2 = ["mango", "durazno"];
   foo.apply({money: "$100"}, arr2);
 
-
+  // the this context and arguments
   var total = sum(10, 20, 30, 40, 100);
-  //console.info("Total = " + total);
+  console.info("Total = " + total);
 
-  //var bob = 13;
-  //// default operator
-  //var weeks = bob || 52;
-  //
-  //
-  //console.log("Bob's weeks = " + weeks);
+  // sidebar: the default operator
+  var bob = 13;
+  // if bob is falsy, it will take the right side of the OR
+  var weeks = bob || 52;
+  console.log("Bob's weeks = " + weeks);
 
-  // invoking functions
 
   // return values from function
+
 
   // scope
 
@@ -94,8 +97,23 @@
 
   // call & apply
 
+  console.info("*******************************************");
 
   // closures
+  function addX(num) {
+    return function (x) {
+      return num + x;
+    }
+  }
+
+  var add10 = addX(10);
+  console.info(add10(25));
+  var add50 = addX(50);
+  console.info(add50(40));
+  console.info(add10(5));
+
+  // first create a modulo function factory
+  // then create an isEven function from it
 
 
 }());
